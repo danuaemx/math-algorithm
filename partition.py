4e727ba8fa65b478d5ac4a0d4dde265db6952f86
Dantiq <148302031+danuaemx@users.noreply.github.com>
2024-06-27 14:21:22 -0600

    Create partition.py

---
+++ partition.py
@@ -0,0 +1,14 @@
+from numba import jit
+
+@jit(nopython=True)
+def partition_p(m,n,p=1,k=2):
+    while k<=m and k<=n:
+        if m==k:
+            p += 1
+        else:
+            p += partition_p(m-k,k)
+        k += 1
+    return p
+
+def partiton(n):
+  return partition_p(n,n)
